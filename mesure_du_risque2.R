5deb0ec066e395206f59247016fa6f65813709b8
Kevin BAMOUNI <kevincedric.y@gmail.com>
2016-12-11 18:40:29 +0100

    un peu de mise en page du code :)

--- mesure_du_risque2.R
+++ mesure_du_risque2.R
@@ -9,16 +9,27 @@ library('matrixStats')
 library('dygraphs')
 
 # Chargement des données
+
 df_airliquide=read.csv('airliquide_jour_2010_2015.csv')
+
 df_axa=read.csv('axa_jour_2010_2015.csv')
+
 df_bnp=read.csv('bnp_jour_2010_2015.csv')
+
 df_compagniesaintgobin=read.csv('compagniesaintgobin_jour_2010_2015.csv')
+
 df_lmvh=read.csv('lmvh_jour_2010_2015.csv')
+
 df_orange=read.csv('orange_jour_2010_2015.csv')
+
 df_sg=read.csv('societegenerale_jour_2010_2015.csv')
+
 df_total=read.csv('total_jour_2010_2015.csv')
+
 df_vinci=read.csv('vinci_jour_2010_2015.csv')
+
 df_creditagricole=read.csv('creditagricole_jour_2010_2015.csv')
+
 date2016 = read.csv('date2016.csv')
 
 # Nombre total d'actifs
@@ -29,19 +40,30 @@ nb_actifs= 100
 portefeuille_parts = c(0.20,0.18,0.18,0.11,0.05,0.099,0.01,0.02,0.12,0.04)
 
 # Portefeuille en nombre d'actifs
+
 portefeuille_nb_actifs = portefeuille_parts*nb_actifs
 
 # Valeurs historiques des actions dans le pportefeuille
 #Après avoir chargé les données on ne retient que deux colonnes, la date et la colonne valeur qui est la moyenne des High et Low
+
 df_vairliquide=data.frame(row.names =as.Date(df_airliquide$Date),Valeur=(df_airliquide$High+df_airliquide$Low)/2)
+
 df_vaxa=data.frame(row.names=as.Date(df_axa$Date),Valeur=(df_axa$High+df_axa$Low)/2)
+
 df_vbnp=data.frame(row.names=as.Date(df_bnp$Date),Valeur=(df_bnp$High+df_bnp$Low)/2)
+
 df_vcompagniesaintgobin=data.frame(row.names=as.Date(df_compagniesaintgobin$Date),Valeur=(df_compagniesaintgobin$High+df_compagniesaintgobin$Low)/2)
+
 df_vlmvh=data.frame(row.names=as.Date(df_lmvh$Date),Valeur=(df_lmvh$High+df_lmvh$Low)/2)
+
 df_vorange=data.frame(row.names=as.Date(df_orange$Date),Valeur=(df_orange$High+df_orange$Low)/2)
+
 df_vsg=data.frame(row.names=as.Date(df_sg$Date),Valeur=(df_sg$High+df_sg$Low)/2)
+
 df_vtotal=data.frame(row.names=as.Date(df_total$Date),Valeur=(df_total$High+df_total$Low)/2)
+
 df_vvinci=data.frame(row.names=as.Date(df_vinci$Date),Valeur=(df_vinci$High+df_vinci$Low)/2)
+
 df_vcreditagricole=data.frame(row.names=as.Date(df_creditagricole$Date),Valeur=(df_creditagricole$High+df_creditagricole$Low)/2)
 
 # historique du Portefeuille en terme de valeur total des actifs...
@@ -89,23 +111,34 @@ vecteur_poids = c(vecteur_poids$airliquide[1],
 
 # Rendement des actifs
 r_airliquide = na.omit(Return.calculate(df_vairliquide))
+
 r_axa = na.omit(Return.calculate(df_vaxa))
+
 r_bnp = na.omit(Return.calculate(df_vbnp))
+
 r_csg = na.omit(Return.calculate(df_vcompagniesaintgobin))
+
 r_crediagricole = na.omit(Return.calculate(df_vcreditagricole))
+
 r_lmvh = na.omit(Return.calculate(df_vlmvh))
+
 r_orange = na.omit(Return.calculate(df_vorange))
+
 r_sg = na.omit(Return.calculate(df_vsg))
+
 r_total = na.omit(Return.calculate(df_vsg))
+
 r_vinci =na.omit(Return.calculate(df_vvinci))
 
 # Matrice des rendements
+
 matrice_rendement = data.frame(row.names = as.Date(row.names(r_airliquide)),airliquide=r_airliquide$Valeur,
                                axa=r_axa$Valeur, bnp=r_bnp$Valeur, csg=r_csg$Valeur, crediagricole = r_crediagricole$Valeur
                                ,lmvh=r_lmvh$Valeur,orange=r_orange$Valeur,sg=r_sg$Valeur,total=r_total,
                                vinci=r_vinci$Valeur)
 
 # Rendement historique du portefeuille
+
 rendement_histo_port = as.data.frame(Return.portfolio(matrice_rendement,vecteur_poids))
 
 dygraph(as.xts(rendement_histo_port), main = "Evolution du rendement du PF", 
@@ -113,6 +146,7 @@ dygraph(as.xts(rendement_histo_port), main = "Evolution du rendement du PF",
 
 # Analyse du portefeuille / rentabilité du portefeuille.
 # Rendement moyen du portefeuille
+
 rendement_moy_port = (mean(r_airliquide$Valeur)*vecteur_poids[1]+mean(x = r_axa$Valeur)*vecteur_poids[2]+
   mean(r_bnp$Valeur)*vecteur_poids[3]+
   mean(r_csg$Valeur)*vecteur_poids[4]+
@@ -154,10 +188,13 @@ jarque.bera.test((as.data.frame(rendement_histo_port))$portfolio.returns)
 
 ################################## VAR ##########################
 # Var historique du porteffeuille 
+
 varhistoriq = VaR(rendement_histo_port,p=0.99,method = "historical")
+
 vargauss = VaR(rendement_histo_port,p=0.99,method = "gaussian")
 
 ### Calcul de la TVAR ####################""""
+
 t_varhistoriq=mean(rendement_histo_port$portfolio.returns[rendement_histo_port$portfolio.returns<rep(varhistoriq,1561)])
 
 
@@ -175,28 +212,37 @@ ar=0
 mat_simu_brownien=matrix(0,261,nsimu)
 
 for(ar in seq(1,nsimu,1)){
+  
 temps=seq(0,1,length=261)
+
 pas=1/260
 
 Bacc = rnorm(260,sd=sqrt(pas))
 
 # Trajectoire
 Bsim = c(0,cumsum(Bacc))
+
 mat_simu_brownien[,ar]=Bsim
   
 plot(temps,Bsim, type="l")
+
 }
 
 # Transformation de la matrice de mouvement brownien en data frame pour ggplot
+
 mat_simu_brownien_df = data.frame(mat_simu_brownien)
 
 ########### Projection du prix du portefeuille par ,St de black and scholes #######################"
 # S0 = 10; 
+
 i = 0
 mat_predict=matrix(0,261,nsimu)
+
 for (i in seq(1,nsimu,1)){
+  
   mat_predict[,i]=portefeuille_historique$Valeur[1562]*exp(sd((as.data.frame(rendement_histo_port))$portfolio.returns)*mat_simu_brownien[,i]+mean((as.data.frame(rendement_histo_port))$portfolio.returns)*(1/255))
-}  
+
+  }  
 
 # Transormation de la matrice de prediction qui contien les prix projet pour chaque mouvement brownien simulé 
 
@@ -206,7 +252,9 @@ matrice_prix_predict_df = data.frame(row.names = as.Date(date2016$Date),mat_pred
 ############### calcul des rendements projetés à partir de la matrice des prix  
 
 Rendement_simu = na.omit(Return.calculate(matrice_prix_predict_df))
+
 var99_predict = colQuantiles(Rendement_simu, probs=(1-0.99))
+
 varminim = min(var99_predict)
 
 ############ Grphique des rendement projetés
@@ -218,11 +266,13 @@ ggplot(Rendement_simu) +
 ############ Graphique des volatilites projetés au lieu de supersossé les 100 colonnes de rendement simuléées ####
 
 ev_volatilite_proj=data.frame(v=(apply(as.matrix(Rendement_simu),2,sd)))
+
 ggplot(ev_volatilite_proj) +
   geom_line(aes(seq(1,nsimu,1), v)) +
   labs(title="Evolution des volatilités simulées", x="rang", y="Volatilité par simulation")
 
 # Volatilité projetée maximale
+
 vola_proj_max = max(ev_volatilite_proj$v)
 
 
@@ -236,19 +286,23 @@ vola_proj_max = max(ev_volatilite_proj$v)
 # S0 = 10; 
 i = 0
 mat_predict2=matrix(0,261,nsimu)
+
 for (i in seq(1,nsimu,1)){
+  
   mat_predict2[,i]=portefeuille_historique$Valeur[1562]*exp(sd((as.data.frame(rendement_histo_port)*2)$portfolio.returns)*mat_simu_brownien[,i]+mean((as.data.frame(rendement_histo_port))$portfolio.returns)*0.2*(1/255))
-}  
+
+  }  
 
 # Transormation de la matrice de prediction qui contien les prix projet pour chaque mouvement brownien simulé 
 
 matrice_prix_predict_df2 = data.frame(row.names = as.Date(date2016$Date),mat_predict2)
 
-
 ############### calcul des rendements projetés à partir de la matrice des prix  
 
 Rendement_simu2 = na.omit(Return.calculate(matrice_prix_predict_df2))
+
 var99_predict2 = colQuantiles(Rendement_simu2, probs=(1-0.99))
+
 varminim2 = min(var99_predict2)
 
 ############ Grphique des rendement projetés
@@ -257,12 +311,12 @@ ggplot(Rendement_simu2) +
   geom_line(aes(seq(1,260,1), X100))+
   labs(title="Evolution des rendements simulées", x="rang", y="Rendement par simulation")
 
-dygraph(as.xts(Rendement_simu2), main = "Evolution du rendement du PF après Stress", 
-        ylab = "Rendement du PF")
+dygraph(as.xts(Rendement_simu2), main = "Evolution du rendement du PF après Stress", ylab = "Rendement du PF")
 
 ############ Graphique des volatilites projetés au lieu de supersossé les 100 colonnes de rendement simuléées ####
 
 ev_volatilite_proj2=data.frame(v=(apply(as.matrix(Rendement_simu2),2,sd)))
+
 ggplot(ev_volatilite_proj2) +
   geom_line(aes(seq(1,nsimu,1), v)) +
   labs(title="Evolution des volatilités simulées après stress", x="rang", y="Volatilité par simulation")
